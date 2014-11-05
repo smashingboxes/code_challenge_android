1dbc2cfea919e6463165143871fad2899f46a8fd
austin@smashingboxes.com <austin@smashingboxes.com>
2014-11-05 13:11:28 -0500

    Initial Commit

---
+++ src/com/smashingboxes/code_challenge_android/MainActivity.java
@@ -0,0 +1,16 @@
+package com.smashingboxes.code_challenge_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
