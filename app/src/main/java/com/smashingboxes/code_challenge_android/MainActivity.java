735594d7ca6616116c7162ca7048f1e4257509af
AustinLanier <austin@smashingboxes.com>
2016-08-29 13:22:07 -0400

    updates build

--- app/src/main/java/com/smashingboxes/code_challenge_android/MainActivity.java
+++ app/src/main/java/com/smashingboxes/code_challenge_android/MainActivity.java
@@ -1,14 +1,12 @@
 package com.smashingboxes.code_challenge_android;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
